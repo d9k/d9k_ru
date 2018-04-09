267bfbd7f91bfd707bf69fe85723d7c97872a81d
d9k <d9k@ya.ru>
2018-04-09 16:07:08 +0300

    session.data debug output

--- controllers/auth.lua
+++ controllers/auth.lua
@@ -1,4 +1,6 @@
 local access = require 'access'
+local session = require 'sailor.session'
+local pformat = require 'thirdparty_libs.pprint'.pformat
 
 --local conf = require 'conf.conf'
 --local access = require 'sailor.access'
@@ -17,13 +19,20 @@ function auth.login(page)
     auth_ok, auth_err = access.login(login, password)
   end
 
-  page:render('login', {login = login, auth_ok = auth_ok, auth_err = auth_err})
+  local session_data_text = session.data
+
+  page:render('login', {
+    login = login,
+    auth_ok = auth_ok,
+    auth_err = auth_err,
+    session_data_text = pformat(session_data)
+  })
 end
 
 function auth.logout(page)
   access.logout()
 
-  page:render('login', {logout = true})
+  page:render('login', {logout = true, session_data = session_data})
 end
 
 return auth
