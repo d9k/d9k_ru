4219d6ff85e026ce4a147f3f829854195e4a292e
d9k <d9k@ya.ru>
2018-04-28 05:04:18 +0300

    db dump: +pgcrypto extension, no GRANT lines

--- db/schema/db.sql
+++ db/schema/db.sql
@@ -28,9 +28,35 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+--
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+
+--
+-- Name: migrations; Type: TABLE; Schema: public; Owner: d9k_local_sailor
+--
+
+CREATE TABLE public.migrations (
+    version character varying NOT NULL,
+    apply_date timestamp without time zone
+);
+
+
+ALTER TABLE public.migrations OWNER TO d9k_local_sailor;
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: adm1104
@@ -44,6 +70,14 @@ CREATE TABLE public.users (
 
 
 ALTER TABLE public.users OWNER TO adm1104;
+
+--
+-- Name: migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: d9k_local_sailor
+--
+
+ALTER TABLE ONLY public.migrations
+    ADD CONSTRAINT migrations_pkey PRIMARY KEY (version);
+
 
 --
 -- Name: users_email_unique; Type: CONSTRAINT; Schema: public; Owner: adm1104
@@ -62,24 +96,31 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+-- PostgreSQL database dump complete
 --
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-GRANT USAGE ON SCHEMA public TO d9k_local_sailor;
-
-
 --
--- Name: TABLE users; Type: ACL; Schema: public; Owner: adm1104
+-- PostgreSQL database dump
 --
 
-REVOKE ALL ON TABLE public.users FROM PUBLIC;
-REVOKE ALL ON TABLE public.users FROM adm1104;
-GRANT ALL ON TABLE public.users TO adm1104;
-GRANT SELECT ON TABLE public.users TO d9k_local_sailor;
+-- Dumped from database version 9.5.12
+-- Dumped by pg_dump version 9.5.12
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: migrations; Type: TABLE DATA; Schema: public; Owner: d9k_local_sailor
+--
+
+COPY public.migrations (version, apply_date) FROM stdin;
+\.
 
 
 --
